7e2eb336a0a5fac2a380a1ba8d5065aa1c4c4200
mnh <martin.hylleberg@systematic.com>
2017-02-10 10:43:47 +0100

    added classification enums

--- src/app/classifications/referaltype.ts
+++
@@ -1,8 +0,0 @@
-export const ALDA00: string = "Ingen henviser";
-export const ALDA11: string = "Akutordning";
-export const ALDA20: string = "Klinisk enhed";
-export const ALDA30: string = "Praktiserende læge";
-export const ALDA31: string = "Praktiserende special læge";
-export const ALDA40: string = "Nyfødt";
-export const ALDA50: string = "Udlandet";
-export const ALDA90: string = "Anden (UNS)";
